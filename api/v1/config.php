d6a66f7c73cb7500dfa06a38ceac79793e064d2f
Kajal Kukreja <kajalkukreja694@gmail.com>
2021-06-03 14:13:15 +0530

    Update config.php

--- api/v1/config.php
+++ api/v1/config.php
@@ -1,5 +1,5 @@
 <?php
     $RECAPTCHA_ENDPOINT="https://www.google.com/recaptcha/api/siteverify";
-    $RECAPTCHA_SECRET_KEY="put your own key;
+    $RECAPTCHA_SECRET_KEY="put your own key";
     $COWIN_ENDPOINT="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/";
 ?>
